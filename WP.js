575f1a513e743a1ed086b51212fee4eda0271b8f
Onolwe <arthur.busuttil@neuf.fr>
2018-05-27 06:53:07 +0200

    bonjour mon nom est rené et voici mon hisoire

---
+++ WP.js
@@ -0,0 +1,36 @@
+//loading needed packages
+const path = require('path');
+const express = require('express');
+const body_parser = require('body-parser');
+
+//method simplified
+const urlencodedParser = body_parser.urlencoded({ extended: false })
+
+//defining hostname and port
+const hostname = '127.0.0.1';
+const port = 3000;
+
+//create object server
+var app = express();
+// allow reading of css files
+app.use(express.static(__dirname));
+
+//run server with function 'Server'
+app.get('/', Server);
+
+//allow to collect data send by the submitted form
+app.post('/', urlencodedParser, Form);
+
+//function runned by server which load file 'index.html' located in the current directory
+function Server(req, res){
+	res.sendFile(path.join(__dirname + '/index.html'));
+}
+
+//the data collection function
+function Form(req, res){
+	var sended = req.body;
+}
+
+//waiting for connection
+app.listen(port, hostname);
+
